3e2d847d9aa32ef8d4623f7e768666a0f6cb1552
andy <andy@ANDY-PC>
2017-05-06 17:47:12 +0800

    1.加入ViewModel->ProductLiteVM 2.Controller新增Action->ListProduct(撈取ProductLiteVM的東東) 3.View新增ListProducts顯示畫面

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using MVC5Course.Models.ViewModel;
 
 namespace MVC5Course.Controllers
 {
@@ -134,5 +135,21 @@ namespace MVC5Course.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult ListProducts()
+        {
+            var data = db.Product
+                .Where(p => p.Active == true)
+                .Select(p => new ProductLiteVM()
+                {
+                    ProductId = p.ProductId,
+                    ProductName = p.ProductName,
+                    Price = p.Price,
+                    Stock = p.Stock
+                })
+                .Take(10);
+            return View(data);
+        }
+
     }
 }
