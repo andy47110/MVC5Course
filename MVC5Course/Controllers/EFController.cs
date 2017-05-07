706247dd5c877648eba93b012b7373ed3a7e97c7
andy <andy@ANDY-PC>
2017-05-07 16:35:07 +0800

    1.修改DB欄位型別IsDeleted->Is刪除 2.新增自訂驗證屬性[商品名稱必須包含Test字串] 3.Model加入自訂驗證屬性 4.新增清單列:已刪除清單

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using System.Data.Entity.Validation;
 
 namespace MVC5Course.Controllers
 {
@@ -26,10 +27,12 @@ namespace MVC5Course.Controllers
             //不同的API傳入的資料，會依據API不同而產生不同的型別
             var one = db.Product.Find(1);
             var two = db.Product.FirstOrDefault(x => x.ProductId == 1);
-            var data = all.Where(x => 
-                    x.Active == true && x.ProductName.Contains("Black")
-                    );
-
+            var data = all.Where(x =>
+                    //方法三
+                    x.Is刪除 == false &&
+                    x.Active == true && x.ProductName.Contains("Black") 
+                    )
+                .OrderByDescending(x => x.ProductId);
             return View(data);
         }
 
@@ -90,12 +93,66 @@ namespace MVC5Course.Controllers
             //}
 
             //方法二
-            db.OrderLine.RemoveRange(product.OrderLine);
+            //db.OrderLine.RemoveRange(product.OrderLine);
+
+            //方法三
+            try
+            {       
+                product.Is刪除 = true;
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                throw ex;
+            }
 
             //如資料為關聯式資料時，下行方法將無法只刪除單一table中的資料
-            db.Product.Remove(product);
-            db.SaveChanges();
+            //db.Product.Remove(product);
+           
+            return RedirectToAction("Index");
+        }
 
+        public ActionResult Details(int id)
+        {
+            var data = db.Database.SqlQuery<Product>(
+                "SELECT * FROM dbo.Product WHERE ProductId=@p0", id).FirstOrDefault();
+            //p0 p1 p2 p3 p4........
+
+
+            return View(data);
+        }
+
+        public ActionResult DeletedDetail()
+        {
+            var all = db.Product.AsQueryable();
+            //延遲載入:
+            //AsQueryable:最後執行時才查詢(延遲載入)
+            //AsEnumerable: 直接查詢，再WHERE...等動作
+            //下方代表只是一個查詢物件
+            //也可轉成AsEnumerable 但會在這邊就把DB物件全部取回來
+            //var allEnum = db.Product.AsEnumerable();
+
+            //不同的API傳入的資料，會依據API不同而產生不同的型別
+            var data = all.Where(x => x.Is刪除 == true && x.ProductName.Contains("Black"))
+                .OrderByDescending(x => x.ProductId);
+
+            return View(data);
+        }
+
+        public ActionResult RestoreData(int id )
+        {
+            var product = db.Product.Find(id);
+
+            try
+            {
+                product.Is刪除 = false;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
             return RedirectToAction("Index");
         }
     }
--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -4,6 +4,7 @@ namespace MVC5Course.Models
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
+    using ValidationAttribute;
 
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
@@ -20,12 +21,15 @@ namespace MVC5Course.Models
     {
         [Required]
         public int ProductId { get; set; }
+
         [Required(ErrorMessage ="請輸入商品名稱")]
-        [MinLength(3), MaxLength(30)]
+        //[MinLength(3), MaxLength(30)]
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         //System.componet Model
         [DisplayName("商品名稱")]
+        [商品名稱必須包含Test字串(ErrorMessage = "商品名稱必須包含Test字串")]
         public string ProductName { get; set; }
+
         [Required(ErrorMessage = "請輸入商品ID")]
         //System.componet Model
         [DisplayName("商品價格")]
